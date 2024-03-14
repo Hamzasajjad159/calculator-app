30c5543fb509d5732387a9f8142b8d353ef4e6cc
hamza.sajjad-at-101380220521 <hamza.sajjad@gosaas.io>
2024-03-15 01:48:23 +0500

    feat: Calculator implementation

---
+++ src/Components/DigitButton/DigitButton.js
@@ -0,0 +1,10 @@
+import { ACTIONS } from "../CalculatorApp/CalculatorApp";
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
